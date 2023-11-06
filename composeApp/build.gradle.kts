adc9772d8ae8f1ce6929f669f0c5caa036da457a
vorgi@mac.com <vorgi@littlebook.speedport.ip>
2023-11-06 21:05:57 +0100

    initial rev

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    
+    alias(libs.plugins.jetbrainsCompose)
+}
+
+kotlin {
+    jvm("desktop")
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        val desktopMain by getting
+        
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+        }
+    }
+}
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.vorgi.experiments"
+            packageVersion = "1.0.0"
+        }
+    }
+}
